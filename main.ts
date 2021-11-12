012fc7e7c6bcaa70162d138ccee24acc951b3cef
igarralmat <93262344+igarralmat@users.noreply.github.com>
2021-11-12 13:55:05 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.forever(function () {
+    if (input.compassHeading() == 0) {
+    	
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+    if (input.compassHeading() == 0) {
+    	
+    } else {
+    	
+    }
+})
